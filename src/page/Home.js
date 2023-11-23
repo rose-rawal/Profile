65136d6176a0ff66b7c7062a8387b0b4d533b8a2
rose <rooserwl@gmail.com>
2023-11-23 10:32:43 +0545

    more changes

--- src/page/Home.js
+++ src/page/Home.js
@@ -4,18 +4,25 @@ import Scrollto from './additional/Scrollto';
 import context from '../components/context/maincontext';
 import { motion } from 'framer-motion';
 import moon from '../image/moon.jpg'
+import { useNavigate } from 'react-router-dom';
 // import image from '../../public/image/stone-wall.jpg'
 const Home = () => {
   const [animation,setAnimation]=useState(true);
   const {bgAnimate,entryAnimation}=useContext(context)
   const [repeat,setRepeat]=useState(true)
-
+  const navigate=useNavigate()
   useEffect(()=>{
     setAnimation(true);
   },[])
   setInterval(()=>{
     setRepeat(!repeat)
   },4000)
+  const gotoPage=(name,e)=>{
+    e.preventDefault();
+    window.scrollTo(0,0)
+    navigate(`/${name}`)
+    
+  }
   // const observer=new IntersectionObserver((entries)=>{
   //   entries.forEach((entry)=>{
   //     if (entry.isIntersecting)
@@ -39,7 +46,7 @@ const Home = () => {
     animate='final'
     exit='exit'
     className={`  w-full text-white bg-transparent`} >
-    <div className={`bg-black transform ${!animation?'-translate-x-full opacity-2':'translate-x-0 opacity-100'}  ease-in-out duration-700 pb-10 min-h-screen pt-36 flex flex-col mb-5`}>
+    <div onClick={(e)=>{gotoPage('',e)}} className={`bg-black transform ${!animation?'-translate-x-full opacity-2':'translate-x-0 opacity-100'}  ease-in-out duration-700 pb-10 min-h-screen pt-36 flex flex-col mb-5`}>
     <h1 className='text-4xl ml-10 pl-20 pb-5'>Hey,</h1>
     <h3 className='text-6xl font-bold transform ease-in-out duration-500 hover:translate-x-10 hover:text-red-400 pl-20 relative self-start'>I'm Rose Rawal <span className='absolute top-14 left-5 text-lg pl-20 hover:text-red-800'>Web Developer</span></h3>
         <p className='py-20 w-1/2 ml-20 animate-fade-in animate-delay-1 duration-1000 transform translate-y-0 pb-10'>Enthusiastic web developer with lots of experience on web development
@@ -50,7 +57,7 @@ animate={{opacity:1}}
 transition={{duration:2,delay:1}}
 className='w-1/4 absolute right-0 -z-20'><img src={moon} alt="" /></motion.div>
 </div>
-<div className='bg-black pt-12 min-h-screen mb-5 one pb-10'>
+<div onClick={(e)=>{gotoPage('project',e)}} className='bg-black pt-12 min-h-screen mb-5 one pb-10'>
 
 <h1 className='text-4xl ml-10 pl-20 pb-5'>I have</h1>
 {/* ${repeat?'opacity-100':'opacity-0'} */}
@@ -59,7 +66,7 @@ className='w-1/4 absolute right-0 -z-20'><img src={moon} alt="" /></motion.div>
 <Carousel/>
 
 </div>
-<div className={`bg-gray-200 pt-12 min-h-screen pb-24 text-black ${bgAnimate===0?'bg-gray-300':bgAnimate===1?'bg-gray-400':bgAnimate===2?'bg-gray-500':bgAnimate===3?'bg-gray-600':'bg-gray-200'}`}>
+<div onClick={(e)=>{gotoPage('skills',e)}} className={`bg-gray-200 pt-12 min-h-screen pb-24 text-black ${bgAnimate===0?'bg-gray-300':bgAnimate===1?'bg-gray-400':bgAnimate===2?'bg-gray-500':bgAnimate===3?'bg-gray-600':'bg-gray-200'}`}>
 
 <h1 className='text-4xl ml-10 pl-20 pb-5'>I learned</h1>
 {/* ${repeat?'opacity-100':'opacity-0'} */}
