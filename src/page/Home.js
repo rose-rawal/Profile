4d3f56f5f4247290772caad4c91d1db0faa15c14
rose <rooserwl@gmail.com>
2023-11-30 14:14:05 +0545

    width manage

--- src/page/Home.js
+++ src/page/Home.js
@@ -49,7 +49,7 @@ const Home = () => {
     <div onClick={(e)=>{gotoPage('',e)}} className={`bg-black transform ${!animation?'-translate-x-full opacity-2':'translate-x-0 opacity-100'}  ease-in-out duration-700 pb-10 min-h-screen pt-36 flex flex-col mb-5`}>
     <h1 className='text-4xl ml-10 pb-5 sm:pl-0 md:pl-10 lg:pl-20'>Hey,</h1>
     <h3 className='text-6xl font-bold transform ease-in-out duration-500 hover:translate-x-10 hover:text-red-400 relative self-start sm:pl-0 md:pl-10 lg:pl-20'>I'm Rose Rawal</h3>
-        <p className='py-20 w-1/2 animate-fade-in animate-delay-1 duration-1000 transform translate-y-0 pb-10 sm:pl-0 md:pl-10 lg:pl-20 sm:w-full'>Enthusiastic web developer with lots of experience on web development
+        <p className='py-20 animate-fade-in animate-delay-1 duration-1000 transform translate-y-0 pb-10 sm:pl-0 md:pl-10 lg:pl-20 sm:w-full md:w-1/2'>Enthusiastic web developer with lots of experience on web development
 through self-ventures. Learned web development with of multiple languages. Each day endears to gain more skilled and acute in web development.</p>
 <motion.div 
 initial={{opacity:0}}
